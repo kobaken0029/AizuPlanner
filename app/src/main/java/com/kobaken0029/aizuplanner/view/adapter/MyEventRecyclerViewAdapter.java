89f9f7409217e6c3820153f3263130587b5a54b2
kobayashi_kento <kobaken0029@gmail.com>
2015-11-23 07:36:58 +0900

    Fragmentのベース作成

---
+++ app/src/main/java/com/kobaken0029/aizuplanner/view/adapter/MyEventRecyclerViewAdapter.java
@@ -0,0 +1,72 @@
+package com.kobaken0029.aizuplanner.view.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.kobaken0029.aizuplanner.R;
+import com.kobaken0029.aizuplanner.view.fragment.EventListFragment.OnListFragmentInteractionListener;
+import com.kobaken0029.aizuplanner.view.adapter.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+
+public class MyEventRecyclerViewAdapter extends RecyclerView.Adapter<MyEventRecyclerViewAdapter.ViewHolder> {
+
+    private final List<DummyItem> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public MyEventRecyclerViewAdapter(List<DummyItem> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_event, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public DummyItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = ButterKnife.findById(mView, R.id.id);
+            mContentView = ButterKnife.findById(mView, R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
