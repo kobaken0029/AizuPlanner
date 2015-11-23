503a0f6fcae881278eeb749f5ff2ba9cce29db4f
kobayashi_kento <kobaken0029@gmail.com>
2015-11-23 12:33:11 +0900

    ApplicationModuleにprovide追加

--- app/src/main/java/com/kobaken0029/aizuplanner/di/module/ApplicationModule.java
+++ app/src/main/java/com/kobaken0029/aizuplanner/di/module/ApplicationModule.java
@@ -3,6 +3,8 @@ package com.kobaken0029.aizuplanner.di.module;
 import android.content.Context;
 
 import com.kobaken0029.aizuplanner.AizuPlannerApplication;
+import com.kobaken0029.aizuplanner.helper.ToolbarHelper;
+import com.kobaken0029.aizuplanner.helper.impl.ToolbarHelperImpl;
 
 import javax.inject.Singleton;
 
@@ -21,5 +23,11 @@ public class ApplicationModule {
     @Singleton
     Context provideApplicationContext() {
         return this.application.getApplicationContext();
+    }
+
+    @Provides
+    @Singleton
+    ToolbarHelper provideToolbarHelper() {
+        return new ToolbarHelperImpl();
     }
 }
