22cfaab0a286ff77d4bc4dc38730425f50c14418
kobayashi_kento <kobaken0029@gmail.com>
2015-11-25 04:49:52 +0900

    Controller作成

---
+++ app/src/main/java/com/kobaken0029/aizuplanner/view/OnFragmentInteractionListener.java
@@ -0,0 +1,5 @@
+package com.kobaken0029.aizuplanner.view;
+
+public interface OnFragmentInteractionListener {
+    void onFragmentInteraction();
+}
---
+++ app/src/main/java/com/kobaken0029/aizuplanner/view/OnListFragmentInteractionListener.java
@@ -0,0 +1,7 @@
+package com.kobaken0029.aizuplanner.view;
+
+import com.kobaken0029.aizuplanner.model.Event;
+
+public interface OnListFragmentInteractionListener {
+    void onListFragmentInteraction(Event item);
+}
