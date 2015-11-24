299afa99139aa56b7f48873f39a3eb460ec2a03d
kobayashi_kento <kobaken0029@gmail.com>
2015-11-25 04:50:23 +0900

    Helper修正

--- app/src/main/java/com/kobaken0029/aizuplanner/helper/ToolbarHelper.java
+++ app/src/main/java/com/kobaken0029/aizuplanner/helper/ToolbarHelper.java
@@ -5,5 +5,5 @@ import android.support.v7.widget.Toolbar;
 import com.kobaken0029.aizuplanner.view.activity.BaseActivity;
 
 public interface ToolbarHelper {
-    void init(final BaseActivity activity, Toolbar toolbar, int titleId, boolean isShowBackArrow);
+    void init(final BaseActivity activity, Toolbar toolbar, int titleId, Toolbar.OnMenuItemClickListener clickListener);
 }
